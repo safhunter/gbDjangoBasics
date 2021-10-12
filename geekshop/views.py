1e4db22aacab663d4e00798e8de8096a81e7ae26
safhunter <safhunter@mail.ru>
2021-10-12 16:20:08 +0500

    Add lesson_2

--- geekshop/views.py
+++ geekshop/views.py
@@ -1,9 +1,60 @@
 from django.shortcuts import render
+import json
+import os
+import io
+from pathlib import Path
+
+
+#  Adds a 'mark' field to each item in the list.
+def add_list_marks(source_list: list):
+    for item in source_list:
+        try:
+            url_path = item['href']
+            if url_path[0] == '\\' or url_path[0] == '/':
+                url_path = url_path[1:]     # todo: add relative link marks
+            if url_path == '':
+                item['mark'] = url_path
+                continue
+            if url_path[-1] != '/' and url_path[-1] != '\\':
+                url_path = url_path + '/'
+            else:
+                url_path[-1] = '/'
+            item['mark'] = url_path
+        except TypeError:
+            continue
+        except KeyError:
+            item['mark'] = ''
+    return source_list
+
+
+def get_menu_context():
+    menu_list = []
+    try:
+        with io.open(os.path.join(Path(__file__).resolve().parent, 'templates', 'geekshop', 'menu_list.json'),
+                     'r', encoding='utf-8-sig') as json_file:
+            menu_list = json.load(json_file)
+    except Exception as ex:
+        print(ex)
+        pass  # todo: add exception handler
+
+    return add_list_marks(menu_list)
 
 
 def main(request):
-    return render(request, 'geekshop/index.html')
+    title = 'Магазин'
+
+    context = {
+        'title': title,
+        'menu_list': get_menu_context(),
+    }
+    return render(request, 'geekshop/index.html', context)
 
 
 def contacts(request):
-    return render(request, 'geekshop/contact.html')
+    title = 'Контакты'
+
+    context = {
+        'title': title,
+        'menu_list': get_menu_context(),
+    }
+    return render(request, 'geekshop/contact.html', context)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from geekshop.views import get_menu_context, add_list_marks
+import json
+import os
+import io
+from pathlib import Path
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'Каталог'
+
+    links_menu = []
+    try:
+        with io.open(os.path.join(Path(__file__).resolve().parent, 'templates', 'mainapp', 'product_menu.json'),
+                     'r', encoding='utf-8-sig') as json_file:
+            links_menu = json.load(json_file)
+    except Exception as ex:
+        print(ex)
+        pass    # todo: add exception handler
+
+    context = {
+        'title': title,
+        'links_menu': add_list_marks(links_menu),
+        'menu_list': get_menu_context(),
+    }
+
+    return render(request, 'mainapp/products.html', context)
